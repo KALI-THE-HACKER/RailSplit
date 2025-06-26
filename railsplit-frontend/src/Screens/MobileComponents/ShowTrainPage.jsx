ee33df4a03f430370677e357690ee8bed481785d
Luckylinux <luckyv0545746@gmail.com>
2025-06-27 03:44:15 +0530

    Add: Sorting indirect trains

--- railsplit-frontend/src/Screens/MobileComponents/ShowTrainPage.jsx
+++ railsplit-frontend/src/Screens/MobileComponents/ShowTrainPage.jsx
@@ -15,8 +15,10 @@ function ShowTrainPage() {
     const [headerHeight, setHeaderHeight] = useState(0);
 
     const [backendStatus, setBackendStatus] = useState("");
+    const [backendStatusType, setBackendStatusType] = useState('')
     const [directTrains, setDirectTrains] = useState([]);
     const [indirectTrains, setIndirectTrains] = useState([]);
+    const [allStations, setAllStations] = useState([]);
 
     // Extract data from react navigate state
     const { origin, destination, trainClass, date } = receivedData?.Data || {};
@@ -37,6 +39,15 @@ function ShowTrainPage() {
                 document.documentElement.classList.add("is-android");
             }
         }
+
+        //Fetching station.json to get the full name of intermediate
+        try{
+            fetch('/stations.json')
+                .then((res) => res.json())
+                .then((data) => setAllStations(data));
+        }catch(err){
+            alert(err);
+        }
     }, []);
 
     // Fetching data from server
@@ -53,7 +64,7 @@ function ShowTrainPage() {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
-                        'x-api-key': 'linuxapi5657'
+                        'x-api-key': import.meta.env.VITE_RAILSPLIT_API_KEY
                     },
                     body: JSON.stringify({
                         origin,
@@ -73,7 +84,15 @@ function ShowTrainPage() {
                 function processApiResponse(data) {
                     if (Array.isArray(data)) {
                         setDirectTrains(data[0]?.["Direct-trains"] || []);
-                        setIndirectTrains(data.slice(1) || []);
+
+                        // Filter out the direct trains object and get only indirect trains
+                        const indirectTrains = data.filter(item => 
+                        item.hasOwnProperty('intermediate'));
+
+                        //Sorting mechanism for indirectTrains Array
+                        indirectTrains.sort((a, b) => ((a.duration || 0) + (a.layover || 15)) - ((b.duration || 0) + b.layover || 15));
+
+                        setIndirectTrains(indirectTrains || []);
                     } else if (data["Direct-trains"]) {
                         setDirectTrains(data["Direct-trains"]);
                         setIndirectTrains([]);
@@ -89,7 +108,15 @@ function ShowTrainPage() {
                         const trimmed = event.data.trim();
                         if (trimmed.startsWith("{") || trimmed.startsWith("[")) {
                             const data = JSON.parse(trimmed);
-                            processApiResponse(data);
+                            //If it's a status message
+                            if(data.status){
+                                setBackendStatus(data.status);
+                                if(data.type){
+                                    setBackendStatusType(data.type);
+                                }
+                            } else { 
+                                processApiResponse(data);
+                            }
                         } else {
                             // Optionally handle non-JSON messages (e.g., status)
                             setBackendStatus(trimmed);
@@ -98,11 +125,13 @@ function ShowTrainPage() {
                         // Optionally log the error
                         alert("Error parsing server message: " + e.message);
                         setBackendStatus("Error parsing server message");
+                        setBackendStatusType("red");
                     }
                 };
 
-                eventSource.onerror = () => {
-                    setBackendStatus("Connection lost");
+                eventSource.onerror = (err) => {
+                    setBackendStatus(`Connection lost! ${err.message}`);
+                    setBackendStatusType("red");
                     eventSource.close();
                 };
 
@@ -121,6 +150,12 @@ function ShowTrainPage() {
         };
         // eslint-disable-next-line
     }, [origin, destination, trainClass, date]);
+    
+    const timeFormatter = (timeInMinutes) => {
+        if(Math.floor(timeInMinutes/60)>=1){
+            return `${Math.floor(timeInMinutes/60)}hr ${timeInMinutes%60}min`
+        }
+    };
 
     return (
         <>
@@ -142,8 +177,12 @@ function ShowTrainPage() {
                         </div>
                     </div>
                 </div>
+                <div className="fixed w-[90vw] mx-[5vw] px-3 h-fit bg-blue-600 text-white text-center rounded-lg z-1" style={{ marginTop: `${headerHeight + 10}px` }}>
+                    <p>{backendStatus}</p>
 
-                <div className="relative flex-1 bg-black h-full w-full overflow-y-auto flex flex-col items-center gap-3 px-4 bottom-5" style={{ marginTop: `${headerHeight + 30}px` }}>
+                </div>
+
+                <div className="relative flex-1 bg-black h-full w-full overflow-y-auto flex flex-col items-center gap-3 px-4 bottom-5" style={{ marginTop: `${headerHeight +30}px` }}>
                     <p className="text-gray-300 bg-black text-center">-:  Direct trains :-</p>
 
                     {directTrains.map((train, index) => (
@@ -174,7 +213,7 @@ function ShowTrainPage() {
                                 <div className="flex flex-col items-end">
                                     <h2 className="text-white text-2xl text-right android-text-20">{train.arrival?.[0]}</h2>
                                     <p className="text-gray-500 android-text-12">{destination?.code}</p>
-                                    <p className="text-gray-500 android-text-12">{train.arrival?.[1]} {train.arrival?.[2]} {train.arrival?.[3]}</p>
+                                    <p className="text-gray-500 android-text-12 text-right">{train.arrival?.[1]} {train.arrival?.[2]} {train.arrival?.[3]}</p>
                                 </div>
                             </div>
                             
@@ -196,6 +235,49 @@ function ShowTrainPage() {
                     ))}
 
                     <p className="text-white bg-black text-center">-:  In-direct trains  :-</p>
+
+                    {indirectTrains.map((train, index) => (
+                        <div key={index} className="bg-[#1c1c1e] h-fit rounded-xl w-full mx-auto px-5 py-3 rounded-box">
+                            <div className="flex flex-row w-full justify-between">
+                                <div className="flex flex-col items-start">
+                                    <h2 className="text-white text-2xl android-text-20">{train.train1_departure_time}</h2>
+                                    <p className="text-gray-500 android-text-12">{origin?.code}</p>
+                                    <p className="text-gray-500 android-text-12 mb-1 text-left">
+                                        {train.train1_departure_date}
+                                    </p>
+                                </div>
+
+                                <div className="flex flex-1 flex-col items-center justify-center gap-6">
+                                    <div className="">via {(allStations.find(st => st.Code === train.intermediate)).Name.charAt(0).toUpperCase() + (allStations.find(st => st.Code === train.intermediate)).Name.slice(1).toLowerCase()}</div>
+                                    <div>
+                                        
+                                    </div>
+                                    <div className="w-full flex items-center justify-center gap-[2px]">
+                                        {Array.from({ length: 17 }).map((_, i) => (
+                                            <div key={i} className="w-[8px] h-[2px] bg-[#3B3F48] rounded"></div>
+                                        ))}
+                                    </div>
+                                    <div className="text-center android-text-12 flex flex-col gap-2">
+                                        <span className="inline-block px-2 py-1 rounded bg-[#23242a] text-green-400 text-xs font-medium">
+                                            Duration: {timeFormatter(train.duration)}
+                                        </span>
+                                        <span className="inline-block px-2 py-1 rounded bg-[#23242a] text-green-400 text-xs font-medium">
+                                            Layover: {timeFormatter(train.layover)}
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col items-end">
+                                    <h2 className="text-white text-2xl text-right android-text-20">{train.train2_arrival_time}</h2>
+                                    <p className="text-gray-500 android-text-1 items-end2">{destination?.code}</p>
+                                    <p className="text-gray-500 android-text-12 mb-1 text-right">
+                                        {train.train2_arrival_date}
+                                    </p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
